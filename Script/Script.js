be551b83904b3c6d71665d0812bd979a9fe9ee47
Larisa Bulacheva <i_lara94@mail.ru>
2019-03-14 13:05:28 +0300

    first

--- Script/Script.js
+++ Script/Script.js
@@ -38,3 +38,5 @@ fetch('https://api.github.com/users/'+userName)
 	div.innerText = "Добавьте в URL: {адрес страницы}?username={имя пользователя}"; 
 }
 
+		//	.catch(function(error) { 
+			//	console.log(error); });
